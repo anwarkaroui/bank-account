0517eb8858fe53810ecd6c18a924b7162152c108
anwarkaroui <120339610+anwarkaroui@users.noreply.github.com>
2026-01-20 10:38:52 +0100

    Create query.sql

---
+++ query.sql
@@ -0,0 +1,13 @@
+UPDATE otc.t_referentialvalue rv
+SET label = CASE
+    WHEN rv.label = 'Visa' THEN 'CbVisa'
+    WHEN rv.label = 'Mastercard' THEN 'CbMaster'
+END
+FROM otc.t_binrange br
+JOIN otc.t_issuer i ON i.id = br.issuer_id
+JOIN otc.t_bank b ON b.id = i.externalbankid
+WHERE br.producttype_id = rv.id
+  AND b.bankcode = '25648'
+  AND br.bincardtrackstart = '4700000000000000000'
+  AND br.bincardtrackend   = '4851111111111111111'
+  AND rv.label IN ('Visa', 'Mastercard');
