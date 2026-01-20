6d983d65a6d89ea93cf8769d282d5c40e867b701
anwarkaroui <120339610+anwarkaroui@users.noreply.github.com>
2026-01-20 10:39:20 +0100

    Update query.sql

--- query.sql
+++ query.sql
@@ -11,3 +11,14 @@ WHERE br.producttype_id = rv.id
   AND br.bincardtrackstart = '4700000000000000000'
   AND br.bincardtrackend   = '4851111111111111111'
   AND rv.label IN ('Visa', 'Mastercard');
+
+
+SELECT rv.id, rv.label, b.bankcode, br.bincardtrackstart, br.bincardtrackend
+FROM otc.t_referentialvalue rv
+JOIN otc.t_binrange br ON br.producttype_id = rv.id
+JOIN otc.t_issuer i ON i.id = br.issuer_id
+JOIN otc.t_bank b ON b.id = i.externalbankid
+WHERE b.bankcode = '25648'
+  AND br.bincardtrackstart = '4700000000000000000'
+  AND br.bincardtrackend   = '4851111111111111111'
+  AND rv.label IN ('Visa', 'Mastercard');
