6fe77be7afc03e6e360f37eee8c5ede1a7f74006
anwarkaroui <120339610+anwarkaroui@users.noreply.github.com>
2026-01-20 18:30:04 +0100

    Update query.sql

--- query.sql
+++ query.sql
@@ -53,3 +53,61 @@ WHERE br.producttype_id = rv.id
   AND br.bincardtrackstart = '4900000000000000000'
   AND br.bincardtrackend   = '4999999999999999999'
   AND rv.label IN ('Visa', 'Mastercard');
+
+SELECT
+  br.id,
+  b.bankcode,
+  br.bincardtrackstart,
+  br.bincardtrackend,
+  br.producttype_id,
+  rv_old.label AS current_label
+FROM otc.t_binrange br
+JOIN otc.t_issuer i ON i.id = br.issuer_id
+JOIN otc.t_bank b ON b.id = i.externalbankid
+JOIN otc.t_referentialvalue rv_old ON rv_old.id = br.producttype_id
+WHERE b.bankcode = :bankcode
+  AND br.bincardtrackstart = :bincardtrackstart
+  AND br.bincardtrackend = :bincardtrackend
+  AND rv_old.label IN ('Visa', 'Mastercard');
+
+SELECT
+  br.id                  AS binrange_id,
+  b.bankcode,
+  br.bincardtrackstart,
+  br.bincardtrackend,
+  rv.id                  AS producttype_id,
+  rv.label               AS producttype_label
+FROM otc.t_binrange br
+JOIN otc.t_issuer i              ON i.id = br.issuer_id
+JOIN otc.t_bank b                ON b.id = i.externalbankid
+JOIN otc.t_referentialvalue rv   ON rv.id = br.producttype_id
+WHERE (
+    (b.bankcode = '12345'
+     AND br.bincardtrackstart = '485454545454545400'
+     AND br.bincardtrackend   = '4854865656999595999')
+ OR
+    (b.bankcode = '25648'
+     AND br.bincardtrackstart = '4900000000000000000'
+     AND br.bincardtrackend   = '4999999999999999999')
+)
+AND rv.label IN ('Visa', 'Mastercard');
+
+UPDATE otc.t_binrange br
+SET producttype_id = CASE
+  WHEN br.producttype_id = 13 THEN 48  -- Visa -> CbVisa
+  WHEN br.producttype_id = 14 THEN 49  -- Mastercard -> CbMaster
+  ELSE br.producttype_id
+END
+FROM otc.t_issuer i
+JOIN otc.t_bank b ON b.id = i.externalbankid
+WHERE br.issuer_id = i.id
+  AND (
+    (b.bankcode = '12345'
+     AND br.bincardtrackstart = '485454545454545400'
+     AND br.bincardtrackend   = '4854865656999595999')
+    OR
+    (b.bankcode = '25648'
+     AND br.bincardtrackstart = '4900000000000000000'
+     AND br.bincardtrackend   = '4999999999999999999')
+  )
+  AND br.producttype_id IN (13, 14);
